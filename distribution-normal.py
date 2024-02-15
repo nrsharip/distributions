e22c3e537b1b75c2364df16adc1bef33bc47e33e
Nail Sharipov <sharipovn@gmail.com>
2024-02-15 15:07:03 +0500

    change the name of variables

--- distribution-normal.py
+++ distribution-normal.py
@@ -5,23 +5,23 @@ from scipy import stats
 import matplotlib.pylab as plt
 import math
 
-DATA = Path().resolve()
+DATA_PATH = Path().resolve()
 
 # Define paths to data sets. If you don't keep your data in the same directory as the code, adapt the path names.
 
-LOANS_INCOME_CSV = DATA / 'data.csv'
+DATA_CSV = DATA_PATH / 'data.csv'
 BINS = 100
 
 ## Sampling Distribution of a Statistic
 
-loans_income = pd.read_csv(LOANS_INCOME_CSV).squeeze('columns')
+raw_data = pd.read_csv(DATA_CSV).squeeze('columns')
 
-intervals = pd.interval_range(0, loans_income.max(), BINS)
+intervals = pd.interval_range(0, raw_data.max(), BINS)
 
 print(intervals)
 
-mean = loans_income.mean()
-std = loans_income.std()
+mean = raw_data.mean()
+std = raw_data.std()
 
 se_05 = std / math.sqrt(5)
 se_20 = std / math.sqrt(20)
@@ -36,7 +36,7 @@ fig, axes = plt.subplots(figsize=(14, 5), ncols=2, nrows=3)
 
 ####### [0,0] #######
 N00, bins00, patches00 = axes[0,0].hist(
-    loans_income,
+    raw_data,
     bins = BINS,
     density=True,
     rwidth=0.8, 
@@ -44,7 +44,7 @@ N00, bins00, patches00 = axes[0,0].hist(
 
 ####### [1,0] #######
 N10, bins10, patches10 = axes[1,0].hist(
-    loans_income,
+    raw_data,
     bins = BINS,
     density=True,
     rwidth=0.8, 
@@ -54,17 +54,17 @@ N10, bins10, patches10 = axes[1,0].hist(
 line05, = axes[0,1].plot([], color='r') 
 # Set the x-axis and y-axis limits to 100 
 axes[0,1].set_xlim(0, 1000) 
-axes[0,1].set_ylim(0, loans_income.max()) 
+axes[0,1].set_ylim(0, raw_data.max()) 
 
-text05 = axes[0,1].text(5, 4*loans_income.max()/5, f'')
+text05 = axes[0,1].text(5, 4*raw_data.max()/5, f'')
 
 ####### [1,1] #######
 line20, = axes[1,1].plot([], color='g') 
 # Set the x-axis and y-axis limits to 100 
 axes[1,1].set_xlim(0, 1000) 
-axes[1,1].set_ylim(0, loans_income.max()) 
+axes[1,1].set_ylim(0, raw_data.max()) 
 
-text20 = axes[1,1].text(5, 3*loans_income.max()/5, f'')
+text20 = axes[1,1].text(5, 3*raw_data.max()/5, f'')
 
 sample_mean_05 = pd.DataFrame(columns = ['mean_05'])
 sample_mean_20 = pd.DataFrame(columns = ['mean_20'])
@@ -85,8 +85,8 @@ Z_PDF = stats.norm(loc=0, scale=1).pdf(Z_X)
 
 # https://www.geeksforgeeks.org/dynamic-visualization-using-python/
 for i in range(1000): 
-    sample_05 = loans_income.sample(5)
-    sample_20 = loans_income.sample(20)
+    sample_05 = raw_data.sample(5)
+    sample_20 = raw_data.sample(20)
 
     bins = [intervals.get_loc(value) for value in sample_05.values]
     for j in range(len(patches00)):
