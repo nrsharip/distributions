db829e97161f85391e0921729da1ee2bac21367d
Nail Sharipov <sharipovn@gmail.com>
2024-03-01 23:51:30 +0500

    Refactor code in binomial distribution

--- distribution-binomial.py
+++ distribution-binomial.py
@@ -10,9 +10,13 @@ import random
 X_RANGE = 1000
 Y_RANGE = 20
 
-distr025 = pd.DataFrame(columns = ['count'])
-distr05 = pd.DataFrame(columns = ['count'])
-distr075 = pd.DataFrame(columns = ['count'])
+P_1 = 0.1
+P_2 = 0.5
+P_3 = 0.8
+
+distr1 = pd.DataFrame(columns = ['count'])
+distr2 = pd.DataFrame(columns = ['count'])
+distr3 = pd.DataFrame(columns = ['count'])
 
 # https://stackoverflow.com/questions/53978121/how-can-i-plot-four-subplots-with-different-colspans
 ax0 = plt.subplot2grid((10, 6), (0, 0), rowspan=5, colspan=2)
@@ -41,16 +45,16 @@ ax2.set_ylabel('count of 1')
 ax2.set_title('Number of successes (p = 0.8)')
 
 # https://stackoverflow.com/questions/42435446/how-to-put-text-outside-of-plots
-text025 = ax0.text(50, 19, '') # , transform=plt.gcf().transFigure
-text025_1 = ax0.text(50, 18, '', color='r', fontweight='bold') # , transform=plt.gcf().transFigure
-text05 = ax1.text(50, 19, '') # , transform=plt.gcf().transFigure
-text05_1 = ax1.text(50, 18, '', color='g', fontweight='bold') # , transform=plt.gcf().transFigure
-text075 = ax2.text(50, 2, '') # , transform=plt.gcf().transFigure
-text075_1 = ax2.text(50, 1, '', color='b', fontweight='bold') # , transform=plt.gcf().transFigure
+text1 = ax0.text(50, 19, '') # , transform=plt.gcf().transFigure
+text1_1 = ax0.text(50, 18, '', color='r', fontweight='bold') # , transform=plt.gcf().transFigure
+text2 = ax1.text(50, 19, '') # , transform=plt.gcf().transFigure
+text2_1 = ax1.text(50, 18, '', color='g', fontweight='bold') # , transform=plt.gcf().transFigure
+text3 = ax2.text(50, 2, '') # , transform=plt.gcf().transFigure
+text3_1 = ax2.text(50, 1, '', color='b', fontweight='bold') # , transform=plt.gcf().transFigure
 
-line025, = ax0.plot([3, 4, 5], color='r', label='p=0.25')
-line05, = ax1.plot([3, 4, 5], color='g', label='p=0.5')
-line075, = ax2.plot([3, 4, 5], color='b', label='p=0.75')
+line1, = ax0.plot([3, 4, 5], color='r', label='p=0.25')
+line2, = ax1.plot([3, 4, 5], color='g', label='p=0.5')
+line3, = ax2.plot([3, 4, 5], color='b', label='p=0.75')
 
 # https://stackoverflow.com/questions/53978121/how-can-i-plot-four-subplots-with-different-colspans
 ax3 = plt.subplot2grid((10, 6), (5, 0), rowspan=5, colspan=6)
@@ -59,56 +63,44 @@ ax0.legend(loc="center right")
 ax1.legend(loc="lower right")
 ax2.legend(loc="center right")
 
-X_025 = np.linspace(0, Y_RANGE, Y_RANGE + 1)
+X = np.linspace(0, Y_RANGE, Y_RANGE + 1)
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.binom.html
-PPF_025 = stats.binom.pmf(X_025, Y_RANGE, 0.1)
-
-X_05 = np.linspace(0, Y_RANGE, Y_RANGE + 1)
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.binom.html
-PPF_05 = stats.binom.pmf(X_05, Y_RANGE, 0.5)
-
-X_075 = np.linspace(0, Y_RANGE, Y_RANGE + 1)
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.binom.html
-PPF_075 = stats.binom.pmf(X_075, Y_RANGE, 0.8)
+PPF_1 = stats.binom.pmf(X, Y_RANGE, P_1)
+PPF_2 = stats.binom.pmf(X, Y_RANGE, P_2)
+PPF_3 = stats.binom.pmf(X, Y_RANGE, P_3)
 
 for i in range(X_RANGE): 
-    p = 0.1
-    sample025 = [1 if r < p else 0 for r in [random.random() for i in range(Y_RANGE)]]
-    # print(i, sample025.count(1), sample025)
-    p = 0.5
-    sample05 = [1 if r < p else 0 for r in [random.random() for i in range(Y_RANGE)]]
-    # print(i, sample05.count(1), sample05)
-    p = 0.8
-    sample075 = [1 if r < p else 0 for r in [random.random() for i in range(Y_RANGE)]]
-    # print(i, sample075.count(1), sample075)
+    sample1 = [1 if r < P_1 else 0 for r in [random.random() for i in range(Y_RANGE)]]
+    sample2 = [1 if r < P_2 else 0 for r in [random.random() for i in range(Y_RANGE)]]
+    sample3 = [1 if r < P_3 else 0 for r in [random.random() for i in range(Y_RANGE)]]
 
-    distr025.loc[i] = sample025.count(1)
-    distr05.loc[i] = sample05.count(1)
-    distr075.loc[i] = sample075.count(1)
+    distr1.loc[i] = sample1.count(1)
+    distr2.loc[i] = sample2.count(1)
+    distr3.loc[i] = sample3.count(1)
 
     if (i < 100) or (i == X_RANGE - 1):
-        text025.set_text(f'{i}: {sample025}')
-        text025_1.set_text(f'Number of "1": {sample025.count(1)}')
-        text05.set_text(f'{i}: {sample05}')
-        text05_1.set_text(f'Number of "1": {sample05.count(1)}')
-        text075.set_text(f'{i}: {sample075}')
-        text075_1.set_text(f'Number of "1": {sample075.count(1)}')
+        text1.set_text(f'{i}: {sample1}')
+        text1_1.set_text(f'Number of "1": {sample1.count(1)}')
+        text2.set_text(f'{i}: {sample2}')
+        text2_1.set_text(f'Number of "1": {sample2.count(1)}')
+        text3.set_text(f'{i}: {sample3}')
+        text3_1.set_text(f'Number of "1": {sample3.count(1)}')
 
-        line025.set_data(distr025.index.values, distr025['count'].values)
-        line05.set_data(distr05.index.values, distr05['count'].values)
-        line075.set_data(distr075.index.values, distr075['count'].values)
+        line1.set_data(distr1.index.values, distr1['count'].values)
+        line2.set_data(distr2.index.values, distr2['count'].values)
+        line3.set_data(distr3.index.values, distr3['count'].values)
 
-        bins025 = distr025["count"].max() - distr025["count"].min()
-        bins05 = distr05["count"].max() - distr05["count"].min()
-        bins075 = distr075["count"].max() - distr075["count"].min()
+        bins1 = distr1["count"].max() - distr1["count"].min()
+        bins2 = distr2["count"].max() - distr2["count"].min()
+        bins3 = distr3["count"].max() - distr3["count"].min()
 
         ax3.cla()
-        ax3.hist(distr025.values, bins = bins025 if bins025 > 0 else 1, density=True, rwidth=0.8, alpha=0.4, color='r', label='sample 20 0.1')
-        ax3.hist(distr05.values, bins = bins05 if bins05 > 0 else 1, density=True, rwidth=0.8, alpha=0.4, color='g', label='sample 20 0.5')
-        ax3.hist(distr075.values, bins = bins075 if bins075 > 0 else 1, density=True, rwidth=0.8, alpha=0.4, color='b', label='sample 20 0.8')
-        ax3.plot(X_025, PPF_025, marker='o', linestyle='dashed', alpha=1.0, color='r', linewidth=2.0)
-        ax3.plot(X_05, PPF_05, marker='o', linestyle='dashed', alpha=1.0, color='g', linewidth=2.0)
-        ax3.plot(X_075, PPF_075, marker='o', linestyle='dashed', alpha=1.0, color='b', linewidth=2.0)
+        ax3.hist(distr1.values, bins = bins1 if bins1 > 0 else 1, density=True, rwidth=0.8, alpha=0.4, color='r', label='sample 20 0.1')
+        ax3.hist(distr2.values, bins = bins2 if bins2 > 0 else 1, density=True, rwidth=0.8, alpha=0.4, color='g', label='sample 20 0.5')
+        ax3.hist(distr3.values, bins = bins3 if bins3 > 0 else 1, density=True, rwidth=0.8, alpha=0.4, color='b', label='sample 20 0.8')
+        ax3.plot(X, PPF_1, marker='o', linestyle='dashed', alpha=1.0, color='r', linewidth=2.0)
+        ax3.plot(X, PPF_2, marker='o', linestyle='dashed', alpha=1.0, color='g', linewidth=2.0)
+        ax3.plot(X, PPF_3, marker='o', linestyle='dashed', alpha=1.0, color='b', linewidth=2.0)
     
         ax3.grid(axis='both', linestyle='--', color='0.95')
         ax3.xaxis.set_major_locator(ticker.MultipleLocator(1))
@@ -127,12 +119,12 @@ for i in range(X_RANGE):
     # plt.pause(0.5 if i < 100 else 0.0001) 
     (i < 100) and plt.pause(0.05)
 
-count025 = pd.cut(distr025["count"], distr025["count"].max() - distr025["count"].min()).value_counts()
-count05 = pd.cut(distr05["count"], distr05["count"].max() - distr05["count"].min()).value_counts()
-count075 = pd.cut(distr075["count"], distr075["count"].max() - distr075["count"].min()).value_counts()
+count1 = pd.cut(distr1["count"], distr1["count"].max() - distr1["count"].min()).value_counts()
+count2 = pd.cut(distr2["count"], distr2["count"].max() - distr2["count"].min()).value_counts()
+count3 = pd.cut(distr3["count"], distr3["count"].max() - distr3["count"].min()).value_counts()
 
-print(count025)
-print(count05)
-print(count075)
+print(count1)
+print(count2)
+print(count3)
 
 plt.show()
